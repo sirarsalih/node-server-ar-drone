c686076402ecb42ae0d6f2955f2c0ab11afc75cb
sirar87 <sirar87@users.noreply.github.com>
2014-08-17 17:03:30 +0200

    Update repl.js

--- src/repl.js
+++ src/repl.js
@@ -2,6 +2,6 @@
  * Created by Sirar on 10.08.2014.
  */
 
-var arDrone = require('C:/Program Files/nodejs/node_modules/ar-drone');
+var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
 client.createRepl();
