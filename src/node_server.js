5ea595e1204875e5bc02e3cb59472f77665cd7de
Chris Searle <chris@chrissearle.org>
2014-09-03 14:03:23 +0200

    Replace coords with mission

--- src/node_server.js
+++ src/node_server.js
@@ -3,105 +3,106 @@
  * Modified by Thuc Hoang on 28.08.2014.
  */
 
+var path = require('path');
+
 var express = require('express');
+
+var arDrone = require('ar-drone');
+var autonomy = require('ardrone-autonomy');
+
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+
 var app = express();
 var port = 1337;
-var arDrone = require('ar-drone');
-
-var autonomy = require('ardrone-autonomy');
-var droneStream = require('dronestream');
-var bodyParser = require('body-parser');
-var path = require('path');
-var _ = require('lodash');
-var server = require("http").createServer(app);
-var client = arDrone.createClient();
 
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'jade');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, '../public')));
 
-var stopAndLand = function() {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var client = arDrone.createClient();
+
+var stopAndLand = function () {
     console.log("Stop and land");
     this.stop();
     this.land();
+};
+
+function renderStatus(response, subtitle) {
+    response.render('status', {
+        title: 'AR Drone',
+        subTitle: subtitle
+    });
 }
 
 app.get('/', function (request, response) {
-    response.render('status', {
-        title: 'AR Drone',
-        subTitle: 'Welcome to my Parrot AR node server!'
-    });
+    client.disableEmergency();
+    renderStatus(response, 'Welcome to my Parrot AR node server!');
 });
 
 app.get('/takeoff', function (request, response) {
     console.log("Taking off...");
     client.takeoff();
 
-    client.after(5000, stopAndLand);
-
-    response.render('status', {
-        title: 'AR Drone',
-        subTitle: 'Take off - done!'
+    client.after(4000, stopAndLand).after(100, function () {
+        renderStatus(response, 'Take off - done!');
     });
 });
 
 app.get('/takeoffAndSpin', function (request, response) {
     console.log("Taking off...");
+
     client.takeoff();
 
     client.after(4000, function () {
         console.log("Spinning clockwise...");
-        this.clockwise(0.5);
-    }).after(1000, stopAndLand);
-
-    response.render('status', {
-        title: 'AR Drone',
-        subTitle: 'Take off and spin - done!'
+        this.clockwise(3);
+    }).after(1000, stopAndLand).after(100, function () {
+        renderStatus(response, 'Take off and spin - done!');
     });
 });
 
-app.get('/takeoffAndFly', function (request, response) {
-    var coordinates = request.query.c;
+app.get('/mission', function (request, response) {
+    var controller = new autonomy.Controller(client, {debug: false});
 
-    if (!(_.isUndefined(coordinates))) {
-        var controller = new autonomy.Controller(client, {debug: false});
+    var mission = new autonomy.Mission(client, controller, {});
 
-        console.log("Taking off...");
-        client.takeoff();
+    mission.takeoff()
+        .zero()       // Sets the current state as the reference
+        .altitude(1)  // Climb to altitude = 1 meter
+        .forward(1)
+        .right(1)
+        .backward(1)
+        .left(1)
+        .hover(1000)  // Hover in place for 1 second
+        .land();
 
-        client.after(8000, function () {
-            var xy;
-            if (coordinates.length > 1) {
-                for (var i = 0; i < coordinates.length; i++) {
-                    var coordinate = coordinates[i];
-                    xy = coordinate.split(",");
-                    console.log("Flying to x=" + xy[0] + " " + "y=" + xy[1]);
-                    controller.go({x: xy[0], y: xy[1]});
-                }
-            } else {
-                xy = coordinates.split(",");
-                console.log("Flying to x=" + xy[0] + " " + "y=" + xy[1]);
-                controller.go({x: xy[0], y: xy[1]});
-            }
-        }).after(1000, stopAndLand);
-    }
+    mission.run(function (err) {
+        if (err) {
+            console.log("Oops, something bad happened: %s", err.message);
+            mission.client().stop();
+            mission.client().land();
 
-    response.render('status', {
-        title: 'AR Drone',
-        subTitle: 'Take off and fly - done!'
+            renderStatus(response, 'Mission - failed!');
+        } else {
+            console.log("Mission success!");
+
+            renderStatus(response, 'Mission - done!');
+        }
     });
 });
 
 app.get('/land', function (request, response) {
     stopAndLand.bind(client)();
 
-    response.render('status', {
-        title: 'AR Drone',
-        subTitle: 'Land - done!'
-    });
+    renderStatus(response, 'Land - done!');
 });
 
-server.listen(port);
-console.log('Node.js express server started on port %s', port);
+app.listen(port, function () {
+    console.log('Node.js express server started on port %s', port);
+});
+
